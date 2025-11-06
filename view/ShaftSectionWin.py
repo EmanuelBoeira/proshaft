fd96767d81e2d11640e61fbc33fc1437e7f2bb09
redbird <boeira050@gmail.com>
2025-11-06 17:50:21 -0300

    mudança nos assets

--- view/ShaftSectionWin.py
+++ view/ShaftSectionWin.py
@@ -28,7 +28,7 @@ class ShaftSectionWindow:
 		self.l.place(x=100, y=140, width=100)
 
 		self.section_img = tk.PhotoImage(file='./assets/section.png')
-		ttk.Label(self.root, image=self.section_img).place(x=250, y=50, width=100, height=100)
+		ttk.Label(self.root, image=self.section_img).place(x=250, y=50, width=150, height=150)
 
 		self.buttonAddSection = tk.Button(self.root, text='Adicionar', command =lambda: [self.AddSection(), self.controller.UpdateSectionTreeview(), self.controller.UpdateCanvas(), self.root.destroy()])
 		self.buttonAddSection.place(x=220, y=240, width=120, height=30)
